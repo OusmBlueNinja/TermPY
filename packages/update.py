316c5c5eaee454b5ab9857a9be24ed58a4201931
OusmBlueNinja <spencer.conlon3@gmail.com>
2023-08-26 22:56:36 -0500

    modified:   packages/pakk.conf 	modified:   packages/update.py

--- packages/update.py
+++ packages/update.py
@@ -1,6 +1,6 @@
 #["upd", "packages.update", ["upd"]]
 # Made By Blurple
-import os, sys, requests
+import os, sys, requests, threading
 
 fileList = [
     "https://raw.githubusercontent.com/OusmBlueNinja/TermPY/main/packages/builtin.py",
@@ -11,13 +11,23 @@ fileList = [
     "https://raw.githubusercontent.com/OusmBlueNinja/TermPY/main/packages/upd.py"
 ]
 
+def download_and_save(url):
+    r = requests.get(url)
+    if 'Requests:' in r.text:
+        print(r.text)
+        print(r.headers['Content-Type'])
+        fileInstallingCreator = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.basename(url))
+        with open(fileInstallingCreator, "w") as newFile:
+            newFile.write(r.text)
+
 def upd(command):
     input(f"Settings: {fileList}, {os.path.dirname(os.path.realpath(__file__))}\nPress enter to continue")
+
+    threads = []
     for url in fileList:
-        r = requests.get(url)
-        if 'Requests:' in r.text:
-            print(r.text)
-            print(r.headers['Content-Type'])
-            fileInstallingCreator = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.basename(url))
-            with open(fileInstallingCreator, "w") as newFile:
-                newFile.write(r.text)
+        thread = threading.Thread(target=download_and_save, args=(url,))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
