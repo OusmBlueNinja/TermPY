3ca7811fea671583e49cfb64b45390297cf90947
OusmBlueNinja <89956790+OusmBlueNinja@users.noreply.github.com>
2023-11-28 22:43:32 -0600

    BETTER Update

--- packages/update.py
+++ packages/update.py
@@ -2,13 +2,27 @@
 # Made By Blurple
 import os, sys, requests, threading, time
 
-fileList = [
-    "https://raw.githubusercontent.com/OusmBlueNinja/TermPY/main/packages/builtin.py",
-    "https://raw.githubusercontent.com/OusmBlueNinja/TermPY/main/packages/nano.py",
-    "https://raw.githubusercontent.com/OusmBlueNinja/TermPY/main/packages/netget.py",
-    "https://raw.githubusercontent.com/OusmBlueNinja/TermPY/main/packages/pip.py",
-    "https://raw.githubusercontent.com/OusmBlueNinja/TermPY/main/packages/update.py"
+url = "https://raw.githubusercontent.com/OusmBlueNinja/TermPY/main/packages/"
+filenames = [
+    "builtin.py",
+    "calc.py",
+    "filemanager.py",
+    "filemanip.py",
+    "hangman.py",
+    "nano.py",
+    "netget.py",
+    "notebook.py",
+    "pip.py",
+    "randomquote.py",
+    "reminder.py",
+    "system_info.py",
+    "test.py",
+    "top.py",
+    "update.py",
+    "vault.py"
 ]
+
+fileList = [f"{url}{filename}" for filename in filenames]
 
 def download_and_save(url):
     r = requests.get(url)
@@ -16,9 +30,14 @@ def download_and_save(url):
     fileInstallingCreator = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.basename(url))
     with open(fileInstallingCreator, "w") as newFile:
         newFile.write(r.text)
+    print("\n")
 
 def upd(command):
-    input(f"Settings: {fileList}, {os.path.dirname(os.path.realpath(__file__))}\nPress enter to continue")
+    print("Programs to download:")
+    for prg in fileList:
+        print(prg)
+    print(f"To path: {os.path.dirname(os.path.realpath(__file__))}\nPress enter to continue")
+    input()
 
     print("Downloading and saving files:")
     
